54ce5ff0f31f98720fcebc54f6402be0e1b9b99c
VVored <muckinvova@gmail.com>
2024-06-30 16:43:22 +0500

    оу да все работает

--- ExpensesCheck/Controller/OperationController.cs
+++ ExpensesCheck/Controller/OperationController.cs
@@ -11,7 +11,7 @@ namespace ExpensesCheck.Controller
 {
     public class OperationController
     {
-        private readonly List<Operation> operations;a
+        private readonly List<Operation> operations;
         public OperationController(List<Operation> operations)
         {
             this.operations = operations;
